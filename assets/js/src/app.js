40d841bab6e922b822d6e7621e84109c6cd8ef4f
KatsuyaEndoh <katsuya@amac.local>
2017-11-07 17:12:10 +0900

    [update] three.js graphic

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -7,49 +7,11 @@
 import Leap from 'leapjs';
 import plugin from 'leapjs-plugins';
 import THREE from 'three';
+import $ from 'jquery';
 
 window.onload = () => {
-
-    // all units in mm
-    var initScene = function () {
-        window.scene = new THREE.Scene();
-        window.renderer = new THREE.WebGLRenderer({
-            alpha: true
-        });
-        window.renderer.setClearColor(0x000000, 0);
-        window.renderer.setSize(window.innerWidth, window.innerHeight);
-        window.renderer.domElement.style.position = 'fixed';
-        window.renderer.domElement.style.top = 0;
-        window.renderer.domElement.style.left = 0;
-        window.renderer.domElement.style.width = '100%';
-        window.renderer.domElement.style.height = '100%';
-        document.body.appendChild(window.renderer.domElement);
-        var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-        directionalLight.position.set(0, 0.5, 1);
-        window.scene.add(directionalLight);
-        window.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-        window.camera.position.fromArray([0, 100, 500]);
-        window.camera.lookAt(new THREE.Vector3(0, 160, 0));
-        window.addEventListener('resize', function () {
-            camera.aspect = window.innerWidth / window.innerHeight;
-            camera.updateProjectionMatrix();
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            renderer.render(scene, camera);
-        }, false);
-        // var geometry = new THREE.CubeGeometry(30, 45, 10);
-        // var material = new THREE.MeshPhongMaterial({color: 0x0000cc});
-        // window.cube = new THREE.Mesh(geometry, material);
-        // cube.position.set(0, 0, 0);
-        // cube.castShadow = true;
-        // cube.receiveShadow = true;
-        // scene.add(cube);
-        renderer.render(scene, camera);
-    };
-    initScene();
-
     // ***** Machine learning *****
-    const types = ['rk', 'sc', 'sn', 'pp', 'al'];
-
+    let types = [0, 1, 2, 3, 4];
     const options = {
         filepaths: {
             model: 'assets/model/rps_model.json',
@@ -60,258 +22,279 @@ window.onload = () => {
     };
     const model1 = new KerasJS.Model(options);
     const model2 = new KerasJS.Model(options);
-
-    // ***** Leap ******
-    Leap.loop({
-        frame: (frame) => {
-            if (frame.hands.length === 2 && frame.historyIdx % 10 === 0) {
-                // console.log(frame);
+    let baseBoneRotation = new THREE.Quaternion().setFromEuler(new THREE.Euler(Math.PI / 2, 0, 0));
+    let renderer, scene, camera;
+    /**
+     *
+     * Leapのコントローラ定義.
+     *
+     **/
+    const leapController = new Leap.Controller();
+    leapController.use('handHold').use('handEntry')
+        .on('frame', (frame) => {
+            if(frame.hands.length === 2) {
                 Promise.all([
                     checkHand(frame.hands[0], 0, model1),
                     checkHand(frame.hands[1], 1, model2)
                 ]).then((result) => {
-                    // TODO
-                    console.log(result);
                     let ary = [0, 1, 2, 3, 4];
                     let centerIdx = Math.ceil(ary.length / 2);
-                    console.log(shiftLeft(ary, centerIdx - result[0]));
-                    // var zeroIsWin = shiftLeft(ary, centerIdx - result[0]).indexOf(result[1]);
                     let zeroIsWin = judge(result[0]).win.indexOf(result[1]) > 0;
-                    console.log(zeroIsWin);
-                    frame.hands.forEach(function (hand, index, array) {
-                        let color = 0x000000;
+                    frame.hands.forEach((hand, index, array) => {
+                        let color = 0xffffff;
                         if (result[0] === result[1]) {
-                            color = 0x000000;
+                            color = 0xffffff;
                         } else if (zeroIsWin && index === 0) {
                             color = 0xff0000;
                         } else if (!zeroIsWin && index === 1) {
                             color = 0xff0000;
                         }
-                        drawHand(scene, hand, index, color);
+                        hand.fingers.forEach((finger) => {
+                            finger.data('jointMeshes').forEach((boneMesh) => {
+                                boneMesh.material.color.set(color);
+                            });
+                        });
+                        hand.fingers.forEach((finger) => {
+                            finger.data('boneMeshes').forEach((boneMesh) => {
+                                boneMesh.material.color.set(color);
+                            });
+                        });
                     });
                 }).catch((err) => {
                     console.log(err);
                 });
             }
-        },
-    })
-        .use('handEntry', function () {
         })
-        .on('handLost', function (hand) {
-        });
-};
+        .on('handFound', (hand) => {
+            hand.fingers.forEach((finger) => {
+                let boneMeshes = [];
+                let jointMeshes = [];
+                finger.bones.forEach((bone) => {
+                    let boneMesh = new THREE.Mesh(
+                        new THREE.CylinderGeometry(3, 3, bone.length),
+                        new THREE.MeshBasicMaterial({
+                            color: 0xff0000,
+                            wireframe: true
+                        })
+                    );
+                    scene.add(boneMesh);
+                    boneMeshes.push(boneMesh);
+                });
 
-// class Finger {
-//     constructor(scene, joints) {
-//         this.scene = scene;
-//         this.joints = joints;
-//         this.object = null;
-//     }
-//
-//     draw() {
-//         if (scene.getObjectByName(this.object.name)) {
-//             scene.remove(this.object);
-//         }
-//
-//         let material = new THREE.LineBasicMaterial({
-//             color: 0x0000ff
-//         });
-//         let geometry = new THREE.Geometry();
-//         this.joints.forEach(function (jointVec) {
-//             geometry.vertices.push(new THREE.Vector3(jointVec[0], jointVec[1], jointVec[2]));
-//         });
-//         scene.add(this.object);
-//         renderer.render(scene, camera);
-//     }
-// }
-//
-// class Hand {
-//     constructor(scene, fingers) {
-//         this.scene = scene;
-//         this.fingers = fingers;
-//         this.object = null;
-//     }
-//
-//     draw() {
-//         this.fingers.forEach(function (finger) {
-//             finger.draw();
-//         });
-//     }
-// }
-
-const getJointsVectors = (finger) => {
-    let jointsVectors = [];
-    finger.bones.forEach(function (bone) {
-        let nextJoint = bone.nextJoint;
-        let prevJoint = bone.prevJoint;
-        jointsVectors.push(nextJoint);
-    });
-    return jointsVectors;
-};
-
-const getFingerVectors = (hand) => {
-    let fingerVectors = [];
-    hand.fingers.forEach(function (finger) {
-        fingerVectors.push(getJointsVectors(finger));
-    });
-    return fingerVectors;
-};
-
-let myHands = [];
-
-const drawHand = (scene, hand, index, color = 0x000000) => {
-
-    let material = new THREE.LineBasicMaterial({
-        color: color
-    });
-    let geometry = new THREE.Geometry();
-    // joints.forEach(function (jointVec) {
-    //     geometry.vertices.push(new THREE.Vector3(jointVec[0], jointVec[1], jointVec[2]));
-    // });
-    getFingerVectors(hand).forEach(function (fingerVecs) {
-        fingerVecs.forEach(function (jointVec) {
-            geometry.vertices.push(new THREE.Vector3(jointVec[0], jointVec[1], jointVec[2]));
-        });
-    });
-
-    let line = new THREE.Line(geometry, material);
-    if (myHands.length === 2) {
-        scene.remove(myHands[index]);
-        myHands[index] = line;
-    } else {
-        myHands.push(line);
-    }
-    scene.add(myHands[index]);
-    renderer.render(scene, camera);
-
-
-    // getFingerVectors(hand).forEach(function (fingerVector, handIndex) {
-    //     let fingers = [];
-    //     getJointsVectors(fingerVector).forEachChunk(function (jointsVector) {
-    //         fingers.push(new Finger(scene, jointsVector));
-    //     });
-    //     if(myHands.length === 2){
-    //         myHands.push(new Hand(scene, fingers));
-    //     }else{
-    //         myHands[index] = new Hand(scene, fingers);
-    //     }
-    // });
-    // myHands.forEach(function (hand) {
-    //     hand.draw();
-    // });
-};
-
-const checkHand = (hand, handIdx, model) => {
-    return new Promise((resolve, reject) => {
-        model.ready().then(
-            () => {
-                const data = calcJointsDist(hand);
-                const inputData = {
-                    'input': new Float32Array(data)
-                };
-                return model.predict(inputData)
-            }).then(
-            (outputData) => {
-                let max = 0;
-                let maxIdx = 0;
-                for (let i = 0; i < outputData.output.length; i++) {
-                    if (max < outputData.output[i]) {
-                        maxIdx = i;
-                        max = outputData.output[i];
-                    }
+                for (let i = 0; i < finger.bones.length + 1; i++) {
+                    let jointMesh = new THREE.Mesh(
+                        new THREE.SphereGeometry(7, 6, 4),
+                        new THREE.MeshBasicMaterial({
+                            color: 0xff0000,
+                            wireframe: true
+                        })
+                    );
+                    scene.add(jointMesh);
+                    jointMeshes.push(jointMesh);
                 }
-                return resolve(maxIdx);
-            }).catch(
-            (err) => {
-                reject(err);
+                finger.data('boneMeshes', boneMeshes);
+                finger.data('jointMeshes', jointMeshes);
             });
-    });
-};
+        })
+        .on('handLost', (hand) => {
+            hand.fingers.forEach((finger) => {
+                let boneMeshes = finger.data('boneMeshes');
+                let jointMeshes = finger.data('jointMeshes');
+                boneMeshes.forEach((mesh) => {
+                    scene.remove(mesh);
+                });
+                jointMeshes.forEach((mesh) => {
+                    scene.remove(mesh);
+                });
+                finger.data({
+                    boneMeshes: null
+                });
+            });
+        });
 
-const calcJointsDist = (hand) => {
-    let dists = [];
-    for (let i = 0; i < hand.fingers.length - 1; i++) {
-        let finger = hand.fingers[i];
-        let nextFinger = hand.fingers[i + 1];
-        let dmpt = [];
-        let dip = finger.dipPosition;
-        let nextDip = nextFinger.dipPosition;
-        let dipDist = calc_distance(dip, nextDip);
-        dmpt.push(dipDist);
-        let mcp = finger.mcpPosition;
-        let nextMcp = nextFinger.mcpPosition;
-        let mcpDist = calc_distance(mcp, nextMcp);
-        dmpt.push(mcpDist);
-        let pip = finger.pipPosition;
-        let nextPip = nextFinger.pipPosition;
-        let pipDist = calc_distance(pip, nextPip);
-        dmpt.push(pipDist);
-        let tip = finger.tipPosition;
-        let nextTip = nextFinger.tipPosition;
-        let tipDist = calc_distance(tip, nextTip);
-        dmpt.push(tipDist);
-        dists.push(dmpt);
-    }
+    const updateHand = (hand) => {
+        hand.fingers.forEach((finger) => {
+            finger.data('boneMeshes').forEach((mesh, i) => {
+                let bone = finger.bones[i];
+                mesh.position.fromArray(bone.center());
+                mesh.setRotationFromMatrix(new THREE.Matrix4().fromArray(bone.matrix()));
+                mesh.quaternion.multiply(baseBoneRotation);
+            });
 
-    let newDists = [];
-    for (let y = 0; y < dists.length; y++) {
-        let row = [];
-        for (let x = 0; x < dists.length; x++) {
-            row.push(0);
+            finger.data('jointMeshes').forEach((mesh, i) => {
+                let bone = finger.bones[i];
+                if (bone) {
+                    mesh.position.fromArray(bone.prevJoint);
+                } else {
+                    bone = finger.bones[i - 1];
+                    mesh.position.fromArray(bone.nextJoint);
+                }
+            });
+
+        });
+    };
+
+    const initScene = () => {
+        let width = $('#render_area').parent().innerWidth();
+        let height = window.innerHeight - $('#render_area').offset().top - 20;
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize(width, height);
+        $('#render_area')[0].appendChild(renderer.domElement);
+        camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+        camera.position.y = 160;
+        camera.position.z = 800;
+        scene = new THREE.Scene();
+        scene.add(new THREE.AxisHelper(400));
+        scene.add(new THREE.GridHelper(400, 10));
+        return scene;
+
+    };
+
+    const addLeapCube = (scene) => {
+        let geometry, material, mesh;
+        geometry = new THREE.CubeGeometry(80, 11.5, 30);
+        material = new THREE.MeshBasicMaterial({
+            color: 0xff0000,
+            wireframe: true
+        });
+        mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+    };
+
+    const animate = () => {
+        window.requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+        leapController.frame(0).hands.forEach(updateHand);
+    };
+    initScene();
+    addLeapCube(scene);
+    leapController.connect();
+    animate();
+
+
+
+
+    const getJointsVectors = (finger) => {
+        let jointsVectors = [];
+        finger.bones.forEach((bone) => {
+            let nextJoint = bone.nextJoint;
+            let prevJoint = bone.prevJoint;
+            jointsVectors.push(nextJoint);
+        });
+        return jointsVectors;
+    };
+
+    const getFingerVectors = (hand) => {
+        let fingerVectors = [];
+        hand.fingers.forEach((finger) => {
+            fingerVectors.push(getJointsVectors(finger));
+        });
+        return fingerVectors;
+    };
+
+    const checkHand = (hand, handIdx, model) => {
+        return new Promise((resolve, reject) => {
+            model.ready().then(
+                () => {
+                    const data = calcJointsDist(hand);
+                    const inputData = {
+                        'input': new Float32Array(data)
+                    };
+                    return model.predict(inputData)
+                }).then(
+                (outputData) => {
+                    let max = 0;
+                    let maxIdx = 0;
+                    for (let i = 0; i < outputData.output.length; i++) {
+                        if (max < outputData.output[i]) {
+                            maxIdx = i;
+                            max = outputData.output[i];
+                        }
+                    }
+                    return resolve(maxIdx);
+                }).catch(
+                (err) => {
+                    reject(err);
+                });
+        });
+    };
+
+    const calcJointsDist = (hand) => {
+        let dists = [];
+        for (let i = 0; i < hand.fingers.length - 1; i++) {
+            let finger = hand.fingers[i];
+            let nextFinger = hand.fingers[i + 1];
+            let dmpt = [];
+            let dip = finger.dipPosition;
+            let nextDip = nextFinger.dipPosition;
+            let dipDist = calc_distance(dip, nextDip);
+            dmpt.push(dipDist);
+            let mcp = finger.mcpPosition;
+            let nextMcp = nextFinger.mcpPosition;
+            let mcpDist = calc_distance(mcp, nextMcp);
+            dmpt.push(mcpDist);
+            let pip = finger.pipPosition;
+            let nextPip = nextFinger.pipPosition;
+            let pipDist = calc_distance(pip, nextPip);
+            dmpt.push(pipDist);
+            let tip = finger.tipPosition;
+            let nextTip = nextFinger.tipPosition;
+            let tipDist = calc_distance(tip, nextTip);
+            dmpt.push(tipDist);
+            dists.push(dmpt);
         }
-        newDists.push(row);
-    }
 
-    for (let y = 0; y < dists.length; y++) {
-        for (let x = 0; x < dists.length; x++) {
-            newDists[y][x] = dists[x][y];
+        let newDists = [];
+        for (let y = 0; y < dists.length; y++) {
+            let row = [];
+            for (let x = 0; x < dists.length; x++) {
+                row.push(0);
+            }
+            newDists.push(row);
         }
-    }
 
-    let data = [];
-    for (let y = 0; y < dists.length; y++) {
-        for (let x = 0; x < dists.length; x++) {
-            data.push(newDists[y][x]);
+        for (let y = 0; y < dists.length; y++) {
+            for (let x = 0; x < dists.length; x++) {
+                newDists[y][x] = dists[x][y];
+            }
         }
-    }
-    return data;
-};
 
-const calc_distance = (v1, v2) => {
-    let dx = v1[0] - v2[0];
-    let dy = v1[1] - v2[1];
-    let dz = v1[2] - v2[2];
-    return Math.sqrt(dx * dx + dy * dy + dz * dz);
-};
-
-let types = [0, 1, 2, 3, 4];
-const judge = (type) => {
-    const center = Math.floor(types.length / 2);
-    const l = shiftLeft(types, type - center);
-    return {
-        'lose': l.slice(0, center),
-        'win': l.slice(center + 1, l.length)
-    }
-};
-
-const shiftLeft = (src, n = 1) => {
-    let dst = src.concat();
-    if (n < 0) {
-        for (let i = 0; i < -n; i++) {
-            dst.unshift(dst.pop());
+        let data = [];
+        for (let y = 0; y < dists.length; y++) {
+            for (let x = 0; x < dists.length; x++) {
+                data.push(newDists[y][x]);
+            }
         }
-    } else {
-        for (let i = 0; i < n; i++) {
-            dst.push(dst.shift());
+        return data;
+    };
+
+    const calc_distance = (v1, v2) => {
+        let dx = v1[0] - v2[0];
+        let dy = v1[1] - v2[1];
+        let dz = v1[2] - v2[2];
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    };
+
+    const judge = (type) => {
+        const center = Math.floor(types.length / 2);
+        const l = shiftLeft(types, type - center);
+        return {
+            'lose': l.slice(0, center),
+            'win': l.slice(center + 1, l.length)
         }
-    }
-    return dst;
+    };
+
+    const shiftLeft = (src, n = 1) => {
+        let dst = src.concat();
+        if (n < 0) {
+            for (let i = 0; i < -n; i++) {
+                dst.unshift(dst.pop());
+            }
+        } else {
+            for (let i = 0; i < n; i++) {
+                dst.push(dst.shift());
+            }
+        }
+        return dst;
+    };
 };
-
-let inArr = [0, 1, 2, 3, 4];
-console.log(inArr);
-let outArr = shiftLeft(inArr, 1);
-console.log('shift 3: ', outArr);
-outArr = shiftLeft(inArr, -1);
-console.log('shift -1: ', outArr);
-
